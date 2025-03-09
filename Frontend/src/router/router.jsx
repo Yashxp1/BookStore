a6737ab137835d05bafe634ac0e84eabc24bf37d
Yash Raj <kyashraj991@gmail.com>
2025-03-09 22:34:53 +0530

    add banner component

--- Frontend/src/router/router.jsx
+++ Frontend/src/router/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App';
+import Home from '../pages/Home/Home'
 
 const router = createBrowserRouter([
   {
@@ -8,7 +9,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>HOME</h1>,
+        element: <Home/>,
       },
       {
         path: '/orders',
