3e004beb8cdd7d6e1a44805be6f1fb5f9da0b2eb
Yash Raj <kyashraj991@gmail.com>
2025-03-10 11:54:38 +0530

    create recommended section

--- Frontend/src/pages/Home/Recommended.jsx
+++ Frontend/src/pages/Home/Recommended.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import BookCard from '../books/BookCard';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Recommended = () => {
   const [books, setBooks] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('Choose a genre');
 
   useEffect(() => {
     fetch('books.json')
@@ -12,8 +19,41 @@ const Recommended = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-3xl font-semibold mb-6 ">Recommended for you</h1>
+    <div className='py-16'>
+      <h2 className="text-3xl font-semibold mb-6 ">Recommended for you</h2>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {books.length > 0 &&
+          books.slice(8,18).map((book, idx) => (
+            <SwiperSlide key={idx}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
